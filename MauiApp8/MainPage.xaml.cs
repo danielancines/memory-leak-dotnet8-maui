a8dec12a060211e8d86ac31fb47f8e7e9e963ce3
Daniel Ancines <daniel.ancines@gmail.com>
2024-06-11 10:19:51 -0300

    First Commit

--- MauiApp8/MainPage.xaml.cs
+++ MauiApp8/MainPage.xaml.cs
@@ -1,26 +1,20 @@
-﻿namespace MauiApp8
+﻿namespace MauiApp8;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    int count = 0;
+
+    public MainPage()
     {
-        int count = 0;
-
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            GC.Collect();
-            var newWindow = new Window(new ContentPage());
-
-            Application.Current?.OpenWindow(newWindow);
-        }
-
-        private void Button_Clicked(object sender, EventArgs e)
-        {
-
-        }
+        InitializeComponent();
     }
 
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        var newWindow = new Window(new MyContentPage());
+
+        Application.Current?.OpenWindow(newWindow);
+    }
 }
+
+public class MyContentPage : ContentPage { }
