1ecedb19517fe95c841562086794c8caadc853f5
Daniel Ancines <daniel.ancines@gmail.com>
2024-06-11 09:50:24 -0300

    Add project files.

---
+++ MauiApp8/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace MauiApp8
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            GC.Collect();
+            var newWindow = new Window(new ContentPage());
+
+            Application.Current?.OpenWindow(newWindow);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
